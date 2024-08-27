761122797159135e5f09c71ca23c34a1f12e21fb
2wndrhs <2wndrhs@gmail.com>
2024-08-28 01:35:54 +0900

    feat: `Server` 구조체의 `new` 연관함수, `run` 메서드 구현

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    fn run(self) {
+        println!("{}", self.addr);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let addr = String::from("127.0.0.1:8080");
+    // Server::new 연관함수의 매개변수인 `addr`로 소유권 이동
+    // -> Server 구조체 인스턴스인 server로 다시 소유권 이동
+    let server = Server::new(addr);
+    server.run();
 }
