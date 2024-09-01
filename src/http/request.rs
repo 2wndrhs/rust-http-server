ab63a5749993999014981bb16ab9b1b8dfd0e288
2wndrhs <2wndrhs@gmail.com>
2024-09-02 02:07:56 +0900

    feat: `Request` 타입에 `TryFrom` 트레이트 구현

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,7 @@
 // super 키워드로 부모 모듈 참조
 use super::method::Method;
+// convert 모듈에 정의된 TryFrom 트레이트
+use std::convert::TryFrom;
 
 #[derive(Debug)]
 // pub 키워드로 구조체를 공개하여도 구조체의 필드는 비공개로 유지됨 -> new 연관 함수를 공개
@@ -16,5 +18,16 @@ impl Request {
             query_string,
             method,
         }
+    }
+}
+
+// Request 타입에 TryFrom 트레이트 구현
+// 표준 라이브러리에서 제공하는 TryFrom 트레이트를 구현함으로써
+// 컴파일러는 TryInto<Request> for &[u8] 또한 구현한다.
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        todo!()
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 // server 모듈
-use std::{io::Read, net::TcpListener};
+use crate::http::Request;
+use std::{convert::TryFrom, io::Read, net::TcpListener};
 pub struct Server {
     addr: String,
 }
@@ -27,7 +28,12 @@ impl Server {
                     // TCP Stream의 read 메서드는 io::Read 트레이트를 구현
                     match stream.read(&mut buffer) {
                         Ok(_) => {
-                            println!("received a request: {}", String::from_utf8_lossy(&buffer))
+                            println!("received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(error) => println!("Failed to parse a request: {error}"),
+                            }
                         }
                         Err(error) => println!("could not read from client: {:?}", error),
                     }
