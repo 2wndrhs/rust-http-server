7ca1f03c459d638d97e7567f5a1c15928d98ad78
2wndrhs <2wndrhs@gmail.com>
2024-12-21 20:45:16 +0900

    feat: send() 메서드의 stream 매개변수를 제네릭 타입으로 변경

--- src/http/response.rs
+++ src/http/response.rs
@@ -16,7 +16,11 @@ impl Response {
         Response { status_code, body }
     }
 
-    pub fn send(&self, stream: &mut TcpStream) -> IoResult<()> {
+    // dyn(동적 디스패치) -> 런타임에 vtable을 참조하여 호출할 메서드가 결정됨
+    // vtable을 참조하여 호출할 메서드를 결정하므로 성능 오버헤드가 발생할 수 있음
+    // impl(정적 디스패치) -> 컴파일 타임에 호출할 메서드가 결정됨
+    // 각 제네릭 인스턴스에 대해 별도의 코드가 생성되므로 바이너리의 크기가 커질 수 있음
+    pub fn send(&self, stream: &mut impl Write) -> IoResult<()> {
         // self.body의 불변 참조를 얻음
         let body = match &self.body {
             Some(b) => b,
