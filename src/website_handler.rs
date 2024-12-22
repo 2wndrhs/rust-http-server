97a1463b5fa70d240e1a11d651c2d34a081a8f80
2wndrhs <2wndrhs@gmail.com>
2024-12-22 20:54:13 +0900

    feat: directory traversal attack을 방지하는 기능 추가

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -16,8 +16,19 @@ impl WebsiteHandler {
 
     fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory traversal attack attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
         // Result 타입을 Option 타입으로 변환
-        fs::read_to_string(path).ok()
     }
 }
 
@@ -27,7 +38,10 @@ impl Handler for WebsiteHandler {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
-                _ => Response::new(StatusCode::NotFound, None),
+                path => match self.read_file(path) {
+                    Some(content) => Response::new(StatusCode::Ok, Some(content)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
             },
             _ => Response::new(StatusCode::NotFound, None),
         }
