5ce888a2dd898bbb0bd69e4b2fdfe92847626f7e
2wndrhs <2wndrhs@gmail.com>
2024-12-22 20:03:22 +0900

    feat: HTML 파일을 읽어 HTTP Response로 제공하도록 수정

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use crate::{
     http::{Method, Request, Response, StatusCode},
     server::Handler,
@@ -11,14 +13,20 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        // Result 타입을 Option 타입으로 변환
+        fs::read_to_string(path).ok()
+    }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
                 _ => Response::new(StatusCode::NotFound, None),
             },
             _ => Response::new(StatusCode::NotFound, None),
