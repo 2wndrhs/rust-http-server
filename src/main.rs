9deae730d500609e91221f08cd79285bee3c7121
2wndrhs <2wndrhs@gmail.com>
2024-12-22 19:53:53 +0900

    feat: WebsiteHandler  구조체에 public_path 환경 변수 추가

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,18 @@ mod server;
 mod website_handler;
 
 use server::Server;
+use std::env;
 use website_handler::WebsiteHandler;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+
+    println!("public_path: {}", public_path);
+
     let addr = String::from("127.0.0.1:8080");
     // Server::new 연관함수의 매개변수인 `addr`로 소유권 이동
     // -> Server 구조체 인스턴스인 server로 다시 소유권 이동
     let server = Server::new(addr);
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -3,7 +3,15 @@ use crate::{
     server::Handler,
 };
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
